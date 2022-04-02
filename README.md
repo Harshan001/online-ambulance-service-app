acedd5f6634e3e39cc329c61d9d5004f8b099f27
Harshan001 <102782311+Harshan001@users.noreply.github.com>
2022-04-02 10:14:55 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# online-ambulance-service-app
+# Online-cab-booking-system
+This project shows a simple method of creating an cab booking system using the basic web development programming languages
+
+# You can see the complete project in htdocs.rar file
---
+++ menu.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title></title>
+								<link rel="stylesheet" type="text/css" href="menu.css"/>
+</head>
+<body background="car.jpg">
+<div class="menu">
+<ul class="one">
+				<a style="text-decoration:none;color:#FFFFFF;" href="index.php"><li class="li">Home </li></a>
+				<a style="text-decoration:none;color:#FFFFFF;"href="about.php"><li class="li">About </li></a>
+				<a style="text-decoration:none;color:#FFFFFF;" href="services.php"><li  class="li">Services</li></a>
+				<a style="text-decoration:none;color:#FFFFFF;" href="contact.php"><li  class="li">Contacts</li></a>
+				</ul>
+			</div>
+			
+		</body>
+</html>
+				
+				
